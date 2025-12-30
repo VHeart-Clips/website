d1117cd3200d3104d829d147209fff9d6a5ed94b
Tawi <Marcel.grochon@gmail.com>
2025-12-30 04:52:24 +0100

    login adding footer / adding lang files

---
+++ lang/en/login.php
@@ -0,0 +1,31 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Pagination Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used by the paginator library to build
+    | the simple pagination links. You are free to change them to anything
+    | you want to customize your views to better match your application.
+    |
+    */
+
+    'title' => 'Welcome to VHeart!',
+    'description' => 'We\'re excited to have you here. Connect your Twitch account to access all features.',
+    'connect_button' => 'Connect with Twitch',
+    'connect_button_aria' => 'Connect with Twitch',
+    'terms_notice' => 'By connecting, you agree to our Terms of Service and Privacy Policy',
+    'community_support' => 'Powered by our amazing community',
+    'logo_alt' => 'VHeart Logo',
+    'privacy' => 'Privacy Policy',
+    'imprint' => 'Imprint',
+    'terms' => 'Terms of Service',
+    'all_rights_reserved' => 'All rights reserved.',
+    'footer_navigation' => 'Footer navigation',
+    'twitter_aria' => 'Twitter',
+    'github_aria' => 'GitHub',
+    'discord_aria' => 'Discord',
+];
