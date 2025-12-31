b7bb9a70b7b8b7207ae8b3e59376723524b00220
Justin K. <justin.k@justplayer.de>
2025-12-31 12:23:03 +0100

    twitch api service

---
+++ app/Services/Twitch/Exceptions/TwitchApiException.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Services\Twitch\Exceptions;
+
+use Exception;
+use GuzzleHttp\Promise\PromiseInterface;
+use GuzzleHttp\Psr7\Response;
+use Illuminate\Http\Client\Promises\LazyPromise;
+
+class TwitchApiException extends Exception
+{
+    public static function GenericApiResponseError(PromiseInterface|LazyPromise|Response $response): TwitchApiException
+    {
+        return new self("Twitch API Error: {$response->getReasonPhrase()} ({$response->getStatusCode()})");
+    }
+
+    public static function ApplicationAuthenticationError(): TwitchApiException
+    {
+        return new self("Could not authenticate app with Twitch");
+    }
+
+    public static function ApplicationClientIdOrSecretNotConfiguredError(): TwitchApiException
+    {
+        return new self("Twitch Client ID or Secret is not configured");
+    }
+}
