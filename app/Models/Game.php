9ee2aff1d86ae42330be5a1e6fbd7956ba2d4ca8
Justin K. <justin.k@justplayer.de>
2025-12-31 23:26:05 +0100

    reformat game and clip model

--- app/Models/Game.php
+++ app/Models/Game.php
@@ -2,19 +2,20 @@
 
 namespace App\Models;
 
+use Database\Factories\GameFactory;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Game extends Model
 {
-    /** @use HasFactory<\Database\Factories\GameFactory> */
+    /** @use HasFactory<GameFactory> */
     use HasFactory;
 
     public $incrementing = false;
 
     public function clips(): HasMany
     {
-        return $this->hasMany(Clip::class,'game_id','id');
+        return $this->hasMany(Clip::class, 'game_id', 'id');
     }
 }
