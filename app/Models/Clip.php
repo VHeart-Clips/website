eefe7fc2afecb986a0dadff007aa4e6a51e8f3b3
Justin K. <justin.k@justplayer.de>
2026-01-01 15:57:09 +0100

    huch

--- app/Models/Clip.php
+++ app/Models/Clip.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use App\Policies\ClipPolicy;
 use Database\Factories\ClipFactory;
+use Illuminate\Database\Eloquent\Attributes\UsePolicy;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
