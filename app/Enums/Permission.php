1887d17aa541f5628623c48736f4e229471c1c99
Justin K. <justin.k@justplayer.de>
2026-01-01 18:44:15 +0100

    some user related permissions

--- app/Enums/Permission.php
+++ app/Enums/Permission.php
@@ -2,9 +2,17 @@
 
 namespace App\Enums;
 
-// could be worth it using integer instead to optimize storage usage lol
 enum Permission: string
 {
-    case Edit_Users = 'edit_users';
-    case Edit_Clips = 'edit_clips';
+    // User
+    case ViewAnyUser = 'view_any_user';
+    case ViewUser = 'view_user';
+    case CreateUser = 'create_user';
+    case UpdateAnyUser = 'update_any_user';
+    case DeleteAnyUser = 'delete_any_user';
+    case RestoreAnyUser = 'restore_any_user';
+    case ForceDeleteAnyUser = 'force_delete_any_user';
+
+    // Non-Model stuff
+    // empty for now
 }
--- app/Policies/UserPolicy.php
+++ app/Policies/UserPolicy.php
@@ -2,6 +2,7 @@
 
 namespace App\Policies;
 
+use App\Enums\Permission;
 use App\Models\User;
 use Illuminate\Auth\Access\Response;
 
@@ -17,7 +18,7 @@ class UserPolicy
      */
     public function viewAny(User $user): bool
     {
-        return true;
+        return $user->can(Permission::ViewAnyUser);
     }
 
     /**
@@ -25,7 +26,7 @@ class UserPolicy
      */
     public function view(User $user, User $model): bool
     {
-        return true;
+        return $user->can(Permission::ViewUser);
     }
 
     /**
@@ -33,7 +34,7 @@ class UserPolicy
      */
     public function create(User $user): bool
     {
-        return true;
+        return $user->can(Permission::CreateUser);
     }
 
     /**
@@ -42,7 +43,7 @@ class UserPolicy
     public function update(User $user, User $model): bool
     {
         // return $user->is($model); // only allow user to edit itself for example
-        return true;
+        return $user->can(Permission::UpdateAnyUser);
     }
 
     /**
@@ -50,7 +51,7 @@ class UserPolicy
      */
     public function delete(User $user, User $model): bool
     {
-        return true;
+        return $user->can(Permission::DeleteAnyUser);
     }
 
     /**
@@ -58,7 +59,7 @@ class UserPolicy
      */
     public function restore(User $user, User $model): bool
     {
-        return true;
+        return $user->can(Permission::RestoreAnyUser);
     }
 
     /**
@@ -66,6 +67,6 @@ class UserPolicy
      */
     public function forceDelete(User $user, User $model): bool
     {
-        return true;
+        return $user->can(Permission::ForceDeleteAnyUser);
     }
 }
