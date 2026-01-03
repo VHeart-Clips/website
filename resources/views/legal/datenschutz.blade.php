9be95a27e873929e06576d36463d6acaef817069
Tawi <Marcel.grochon@gmail.com>
2026-01-03 19:10:03 +0100

    removed import / added vite path

--- resources/views/legal/datenschutz.blade.php
+++ resources/views/legal/datenschutz.blade.php
@@ -4,9 +4,6 @@
     <meta charset="UTF-8">
     <title>Datenschutz – VHeart</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100..900&display=swap" rel="stylesheet">
     <style>
         *, *::before, *::after {
             box-sizing: border-box;
