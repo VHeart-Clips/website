3b6352fbb16d67ef7b6f9a45110b25595b491553
Justin K. <justin.k@justplayer.de>
2026-01-06 08:30:41 +0100

    easy way to get the formatted box art for a game/category

--- app/Models/Game.php
+++ app/Models/Game.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Models;
 
 use Database\Factories\GameFactory;
@@ -14,8 +16,17 @@ class Game extends Model
 
     public $incrementing = false;
 
+    public const PLACEHOLDER_BOX_ART = "https://static-cdn.jtvnw.net/ttv-static/404_boxart-{width}x{height}.jpg";
+
     public function clips(): HasMany
     {
         return $this->hasMany(Clip::class, 'game_id', 'id');
+    }
+
+    public function getBoxArt($width = 188, $height = 250): ?string
+    {
+        $boxArtUrl = $this->box_art ?? self::PLACEHOLDER_BOX_ART;
+
+        return str_replace(['{width}', '{height}'], [$width, $height], $boxArtUrl);
     }
 }
