bbbb90ec8bb7f8705991cccff5e9af861d343891
Justin K. <justin.k@justplayer.de>
2026-01-13 07:26:39 +0100

    admin category resource translation

---
+++ lang/en/admin/resources/categories.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    'table' => [
+        'columns' => [
+            'box_art' => 'Box Art',
+            'title' => 'Title',
+        ],
+    ],
+];
