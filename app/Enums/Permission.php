ae631bbe2241169a7edc5e68901de6d969cb10c2
Justin K. <justin.k@justplayer.de>
2026-01-15 05:55:35 +0100

    add permissions for reports
    they are only really used for backend stuff, everyone can report things in the frontend but only people with permissions should work on them

--- app/Enums/Permission.php
+++ app/Enums/Permission.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Enums;
 
 enum Permission: string
@@ -12,6 +14,15 @@ enum Permission: string
     case DeleteAnyUser = 'delete_any_user';
     case RestoreAnyUser = 'restore_any_user';
     case ForceDeleteAnyUser = 'force_delete_any_user';
+
+    // Report
+    case ViewAnyReport = 'view_any_report';
+    case ViewReport = 'view_report';
+    case CreateReport = 'create_report';
+    case UpdateAnyReport = 'update_any_report';
+    case DeleteAnyReport = 'delete_any_report';
+    case RestoreAnyReport = 'restore_any_report';
+    case ForceDeleteAnyReport = 'force_delete_any_report';
 
     // Non-Model stuff
     // empty for now
