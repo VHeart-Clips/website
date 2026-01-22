75b964466548347ad6706789f0dc5de5b43fea50
Justin K. <justin.k@justplayer.de>
2026-01-22 07:02:14 +0100

    locales rate limiting

--- routes/web.php
+++ routes/web.php
@@ -71,7 +71,9 @@ Route::middleware(['auth', 'verified'])->group(function () {
     })->name('about');
 });
 
-Route::get('/locales.json', Locales::class)->name('locales');
+Route::get('/locales.json', Locales::class)
+    ->middleware(['throttle:locales'])
+    ->name('locales');
 
 Route::get('/locales/{lang}', static function (Request $request, $lang) {
     if (! array_key_exists($lang, Config::get('app.locales'))) {
