c576846c49973920a3d6969d08668af4b8a20e44
Justin K. <justin.k@justplayer.de>
2026-01-23 10:37:33 +0100

    a script to fix them all

---
+++ fix.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+./vendor/bin/sail down
+
+echo "Deleting stuff that may be broken..."
+rm -rf ./vendor
+rm ./bootstrap/cache/packages.php
+rm ./bootstrap/cache/services.php
+rm ./frankenphp
+rm ./.phpstorm.meta.php
+rm ./_ide_helper.php
+
+echo "Installing Composer dependencies..."
+docker run --rm \
+    --interactive \
+    --tty \
+    --volume "$PWD":/app \
+    --user "$(id -u):$(id -g)" \
+    composer install --ignore-platform-reqs
+
+echo "Starting Sail..."
+./vendor/bin/sail up -d
+
+until ./vendor/bin/sail ps | grep "laravel.test" | grep -q "(healthy)"; do
+    echo "waiting for sail to be healthy..."
+    sleep 3
+done
+
+echo "Resetting stuff..."
+
+./vendor/bin/sail artisan optimize:clear
+./vendor/bin/sail composer install
+./vendor/bin/sail artisan migrate:fresh --seed
+./vendor/bin/sail composer helper
+./vendor/bin/sail down
+
+echo "everything should be fixed now"
