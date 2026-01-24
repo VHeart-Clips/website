efd089b90b76064cc8134bf0995a95a6f608341d
Justin K. <justin.k@justplayer.de>
2026-01-24 13:02:13 +0100

    improve fix script to contain more files that may be stale

--- fix.sh
+++ fix.sh
@@ -10,6 +10,15 @@ rm ./bootstrap/cache/services.php
 rm ./frankenphp
 rm ./.phpstorm.meta.php
 rm ./_ide_helper.php
+rm ./public/hot
+rm ./public/frankenphp-worker.php
+rm -rf ./public/css/filament
+rm -rf ./public/fonts/filament
+rm -rf ./public/js/filament
+rm -rf ./resources/js/actions
+rm -rf ./resources/js/routes
+rm -rf ./resources/js/wayfinder
+rm -rf ./public/build
 
 echo "Installing Composer dependencies..."
 docker run --rm \
@@ -30,13 +39,15 @@ until ./vendor/bin/sail ps | grep "laravel.test" | grep -q "(healthy)"; do
     sleep 3
 done
 
-echo "Resetting stuff..."
+echo "(re)Installing dependencies using sail..."
 
-./vendor/bin/sail artisan optimize:clear
 ./vendor/bin/sail composer install
+./vendor/bin/sail npm install
+
+echo "Resetting stuff..."
+./vendor/bin/sail artisan optimize:clear
 ./vendor/bin/sail artisan migrate:fresh --seed
 ./vendor/bin/sail composer helper
-./vendor/bin/sail npm install
 ./vendor/bin/sail down
 
 echo "everything should be fixed now"
