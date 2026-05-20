c3f6fc4104037569d0b33a570f2cdf68127e96c9
Justin K. <justin.k@justplayer.de>
2026-05-20 11:58:07 +0200

    update short url migration to support all possible urls and index on slug

--- database/migrations/2026_05_20_085346_create_short_urls_table.php
+++ database/migrations/2026_05_20_085346_create_short_urls_table.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -8,10 +10,10 @@ return new class extends Migration
 {
     public function up(): void
     {
-        Schema::create('short_urls', function (Blueprint $table) {
+        Schema::create('short_urls', function (Blueprint $table): void {
             $table->id();
-            $table->string('slug');
-            $table->string('url');
+            $table->string('slug')->index();
+            $table->string('url', 2048);
             $table->unsignedBigInteger('clicks')->default(0);
             $table->timestamps();
         });
