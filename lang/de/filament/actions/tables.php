70cf65615954a20df4b55778b95ccce102918e42
Justin K. <justin.k@justplayer.de>
2026-05-25 01:12:48 +0200

    german translation for update clip status action

---
+++ lang/de/filament/actions/tables.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    'clips' => [
+        'update_clip_status' => [
+            'label' => 'Clip Status Ändern',
+            'form' => [
+                'status' => 'Status',
+            ],
+        ],
+    ],
+];
