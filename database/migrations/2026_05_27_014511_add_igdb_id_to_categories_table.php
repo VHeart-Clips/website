47cfddee86fa38dba35af0d9baec35098e89b26d
Justin K. <justin.k@justplayer.de>
2026-05-27 01:49:51 +0200

    add nullable but unique igdb_id to category table

---
+++ database/migrations/2026_05_27_014511_add_igdb_id_to_categories_table.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::table('categories', function (Blueprint $table) {
+            $table->unsignedBigInteger('igdb_id')->after('id')->nullable()->unique();
+        });
+    }
+};
