04d8e16fc40d60f238e7f088ea973dc97bc67d22
Speidy674 <marvin33@live.de>
2026-05-26 22:05:57 +0200

    update Dashboard Title

--- app/Filament/Dashboard/Pages/Dashboard.php
+++ app/Filament/Dashboard/Pages/Dashboard.php
@@ -6,7 +6,9 @@ namespace App\Filament\Dashboard\Pages;
 
 use App\Enums\Filament\LucideIcon;
 use BackedEnum;
+use Filament\Facades\Filament;
 use Filament\Pages\Dashboard as BaseDashboard;
+use Illuminate\Contracts\Support\Htmlable;
 
 class Dashboard extends BaseDashboard
 {
@@ -15,5 +17,15 @@ class Dashboard extends BaseDashboard
     public static function getNavigationLabel(): string
     {
         return __('dashboard/navigation.dashboard');
+    }
+
+    public function getTitle(): string|Htmlable
+    {
+        return Filament::getTenant()->name.' - '.self::getNavigationLabel();
+    }
+
+    public function getHeading(): string|Htmlable|null
+    {
+        return self::getNavigationLabel();
     }
 }
