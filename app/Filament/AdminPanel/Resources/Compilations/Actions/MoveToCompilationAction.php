f7e454391673d42d87971172b485c8ab2411eb8a
Justin K. <justin.k@justplayer.de>
2026-05-29 01:57:31 +0200

    make move to compilation action a bit more robust against possible issues

--- app/Filament/AdminPanel/Resources/Compilations/Actions/MoveToCompilationAction.php
+++ app/Filament/AdminPanel/Resources/Compilations/Actions/MoveToCompilationAction.php
@@ -8,8 +8,10 @@ use App\Enums\Clips\CompilationClipClaimStatus;
 use App\Enums\Clips\CompilationStatus;
 use App\Enums\Filament\LucideIcon;
 use App\Models\Clip;
+use Exception;
 use Filament\Actions\Action;
 use Filament\Forms\Components\Select;
+use Filament\Notifications\Notification;
 use Illuminate\Database\Eloquent\Builder;
 
 class MoveToCompilationAction extends Action
@@ -37,10 +39,37 @@ class MoveToCompilationAction extends Action
                     ->required(),
             ])
             ->action(function (Clip $record, array $data, $livewire): void {
-                $record->compilations()
-                    ->updateExistingPivot($livewire->getOwnerRecord()->id, [
-                        'compilation_id' => $data['compilation_id'],
-                    ]);
+                $alreadyExists = $record->compilations()
+                    ->wherePivot('compilation_id', $data['compilation_id'])
+                    ->exists();
+
+                if ($alreadyExists) {
+                    Notification::make('already-in-compilation')
+                        ->title('Could not move Clip')
+                        ->body('Clip is already in the selected Compilation')
+                        ->warning()
+                        ->send();
+
+                    $this->halt();
+                }
+
+                try {
+                    $record->compilations()
+                        ->newPivotStatement()
+                        ->where('compilation_id', $livewire->getOwnerRecord()->id)
+                        ->where('clip_id', $record->id)
+                        ->update(['compilation_id' => $data['compilation_id']]);
+                } catch (Exception $e) {
+                    report($e);
+
+                    Notification::make('already-in-compilation')
+                        ->title('Could not move Clip')
+                        ->body('There was an error while moving clip, please contact IT if the issue persists')
+                        ->danger()
+                        ->send();
+
+                    $this->halt();
+                }
             })
             ->successNotificationTitle(__('admin/resources/compilations.relation_managers.clips.notifications.moved_to_compilation'));
     }
