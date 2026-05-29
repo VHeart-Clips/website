ed61f82821c13a73491ecf122ee0b7c797f5d1ea
Justin K. <justin.k@justplayer.de>
2026-05-29 16:10:14 +0200

    fix implicit float to int cast warning in CompilationsTable

--- app/Filament/AdminPanel/Resources/Compilations/Tables/CompilationsTable.php
+++ app/Filament/AdminPanel/Resources/Compilations/Tables/CompilationsTable.php
@@ -70,7 +70,7 @@ class CompilationsTable
                     ->sortable(),
 
                 TextColumn::make('clips_sum_duration')
-                    ->formatStateUsing(fn (float $state): string => gmdate('i:s', (int) $state))
+                    ->formatStateUsing(fn (float $state): string => gmdate('i:s', (int) round($state)))
                     ->label('admin/resources/compilations.table.columns.clips_sum_duration')
                     ->sum('clips', 'duration')
                     ->placeholder('No Clips :(')
@@ -79,7 +79,7 @@ class CompilationsTable
                     ->alignCenter()
                     ->sortable(),
                 TextColumn::make('clips_est_duration')
-                    ->formatStateUsing(fn (float $state): string => gmdate('i:s', (int) floor($state * 0.6)))
+                    ->formatStateUsing(fn (float $state): string => gmdate('i:s', (int) round($state)))
                     ->label('admin/resources/compilations.table.columns.clips_est_duration')
                     ->sum('clips as clips_est_duration', 'duration')
                     ->toggleable(isToggledHiddenByDefault: true) // hidden by default because its likely very off, but nice to have i guess
@@ -90,7 +90,7 @@ class CompilationsTable
                     ->sortable(),
                 TextColumn::make('clips_avg_duration')
                     ->label('admin/resources/compilations.table.columns.clips_avg_duration')
-                    ->formatStateUsing(fn (int $state): string => round($state).'s')
+                    ->formatStateUsing(fn (float $state): string => round($state).'s')
                     ->avg('clips', 'duration')
                     ->placeholder('No Clips :(')
                     ->fontFamily(FontFamily::Mono)
