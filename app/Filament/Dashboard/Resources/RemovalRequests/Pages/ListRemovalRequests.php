388bf6b1fb62020e3f9e54ea6155bfb5f4e34625
Justin K. <justin.k@justplayer.de>
2026-06-01 18:58:16 +0200

    add tenant prefix to removal request resource title

--- app/Filament/Dashboard/Resources/RemovalRequests/Pages/ListRemovalRequests.php
+++ app/Filament/Dashboard/Resources/RemovalRequests/Pages/ListRemovalRequests.php
@@ -6,11 +6,18 @@ namespace App\Filament\Dashboard\Resources\RemovalRequests\Pages;
 
 use App\Filament\Dashboard\Resources\RemovalRequests\RemovalRequestResource;
 use Filament\Actions\CreateAction;
+use Filament\Facades\Filament;
 use Filament\Resources\Pages\ListRecords;
+use Illuminate\Contracts\Support\Htmlable;
 
 class ListRemovalRequests extends ListRecords
 {
     protected static string $resource = RemovalRequestResource::class;
+
+    public function getTitle(): string|Htmlable
+    {
+        return Filament::getTenant()->name.' - '.parent::getTitle();
+    }
 
     protected function getHeaderActions(): array
     {
