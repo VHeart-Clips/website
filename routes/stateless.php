b34bdb663d451bb9959fede84515485377c7ae3a
Justin K. <justin.k@justplayer.de>
2026-06-03 01:51:03 +0200

    move go.vheart.net route to stateless router

--- routes/stateless.php
+++ routes/stateless.php
@@ -1,3 +1,11 @@
 <?php
 
 declare(strict_types=1);
+use App\Http\Controllers\RedirectController;
+
+Route::domain('go.vheart.net')
+    ->group(function () {
+        Route::get('{slug?}', RedirectController::class)
+            ->name('shorturl.redirect')
+            ->where('slug', '.*');
+    });
