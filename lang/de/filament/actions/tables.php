984f1d90ec8c9ab7cfe1ad7c98ac07dc706456ad
Justin K. <justin.k@justplayer.de>
2026-06-04 09:01:52 +0200

    (german) add filament/inputs/tables translations

--- lang/de/filament/actions/tables.php
+++ lang/de/filament/actions/tables.php
@@ -4,10 +4,27 @@ declare(strict_types=1);
 
 return [
     'clips' => [
+        'submit_action' => [
+            'label' => 'Clip Einsenden',
+            'form' => [
+                'uri' => 'Clip URI',
+                'tags' => 'Tags',
+                'bypass' => [
+                    'label' => 'Limitierungen Ignorieren',
+                    'description' => 'Du hast die möglichkeit bestimmte Limitierungen zu ignorieren, bitte nicht ausnutzen.',
+                    'options' => [
+                        'broadcaster_consent' => 'Broadcaster Zustimmung',
+                        'category_ban' => 'Kategorie Seiten-Bann',
+                        'minimum_length' => 'Minimale Länge',
+                        'maximum_age' => 'Maximales Alter',
+                    ],
+                ],
+            ],
+        ],
         'update_clip_status' => [
             'label' => 'Clip Status Ändern',
             'form' => [
-                'status' => 'Status',
+                'status' => 'Clip Status',
             ],
         ],
     ],
