0443d8c8a1d51aca1265c5669067398391a151ee
Justin K. <justin.k@justplayer.de>
2026-06-05 00:57:27 +0200

    allow returning Access Response in BannablePolicy

--- app/Policies/Contracts/BannablePolicy.php
+++ app/Policies/Contracts/BannablePolicy.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace App\Policies\Contracts;
 
 use App\Models\User;
+use Illuminate\Auth\Access\Response;
 use Illuminate\Database\Eloquent\Model;
 
 interface BannablePolicy
@@ -12,10 +13,10 @@ interface BannablePolicy
     /**
      * can $user ban the model
      */
-    public function ban(User $user, Model $model): bool;
+    public function ban(User $user, Model $model): Response|bool;
 
     /**
      * can $user unban the model
      */
-    public function unban(User $user, Model $model): bool;
+    public function unban(User $user, Model $model): Response|bool;
 }
