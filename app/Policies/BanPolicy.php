559526fc0f6b35471aac96850d4dec40d99d6ddb
Justin K. <justin.k@justplayer.de>
2026-06-05 00:37:25 +0200

    create ban related policy for global ban resource

---
+++ app/Policies/BanPolicy.php
@@ -0,0 +1,67 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Policies;
+
+use App\Enums\Permission;
+use App\Models\Ban;
+use App\Models\User;
+
+class BanPolicy
+{
+    public function viewAny(User $user): bool
+    {
+        return $user->can(Permission::ViewAnyBan);
+    }
+
+    public function view(User $user, Ban $ban): bool
+    {
+        return $user->can(Permission::ViewAnyBan);
+    }
+
+    public function create(User $user): bool
+    {
+        return $user->can(Permission::CreateBan);
+    }
+
+    public function update(User $user, Ban $ban): bool
+    {
+        return $user->can(Permission::UpdateAnyBan);
+    }
+
+    public function updateAny(User $user): bool
+    {
+        return $user->can(Permission::UpdateAnyBan);
+    }
+
+    public function delete(User $user, Ban $ban): bool
+    {
+        return $user->can(Permission::DeleteAnyBan);
+    }
+
+    public function deleteAny(User $user): bool
+    {
+        return $user->can(Permission::DeleteAnyBan);
+    }
+
+    public function restore(User $user, Ban $ban): bool
+    {
+        return false;
+    }
+
+    public function restoreAny(User $user): bool
+    {
+        return false;
+    }
+
+    public function forceDelete(User $user, Ban $ban): bool
+    {
+        return false;
+    }
+
+    public function forceDeleteAny(User $user): bool
+    {
+        return false;
+    }
+}
