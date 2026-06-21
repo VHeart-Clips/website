95fc57d68eff1c278e672f78bca0e9e98eb5fb97
Justin K. <justin.k@justplayer.de>
2026-06-21 21:18:20 +0200

    implement canAccess on broadcaster dashboard page
    we could probably add a Dashboard permission later

--- app/Filament/Dashboard/Pages/Dashboard.php
+++ app/Filament/Dashboard/Pages/Dashboard.php
@@ -4,15 +4,25 @@ declare(strict_types=1);
 
 namespace App\Filament\Dashboard\Pages;
 
+use App\Enums\Broadcaster\BroadcasterPermission;
 use App\Enums\Filament\LucideIcon;
 use BackedEnum;
 use Filament\Facades\Filament;
 use Filament\Pages\Dashboard as BaseDashboard;
 use Illuminate\Contracts\Support\Htmlable;
+use Illuminate\Support\Facades\Gate;
 
 class Dashboard extends BaseDashboard
 {
     protected static string|BackedEnum|null $navigationIcon = LucideIcon::House;
+
+    public static function canAccess(): bool
+    {
+        return array_any(
+            BroadcasterPermission::cases(),
+            fn ($permission) => Gate::allows('dashboardAccess', [Filament::getTenant(), $permission])
+        );
+    }
 
     public static function getNavigationLabel(): string
     {
