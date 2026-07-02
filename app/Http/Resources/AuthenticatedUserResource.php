243b44b54cd2c54e1762e4a5640b9d8e093c9d15
Justin K. <justin.k@justplayer.de>
2026-07-02 20:13:01 +0200

    remove user model mixin on AuthenticatedUserResource

--- app/Http/Resources/AuthenticatedUserResource.php
+++ app/Http/Resources/AuthenticatedUserResource.php
@@ -4,14 +4,11 @@ declare(strict_types=1);
 
 namespace App\Http\Resources;
 
-use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
 /**
  * Authenticated User Data
- *
- * @mixin User
  */
 class AuthenticatedUserResource extends JsonResource
 {
