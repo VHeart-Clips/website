e8b3476aa6931303e1d61c7847453996c8be8d32
Speidy674 <marvin33@live.de>
2026-08-15 15:50:18 +0200

    fix typo

--- lang/de/vote.php
+++ lang/de/vote.php
@@ -5,6 +5,6 @@ declare(strict_types=1);
 return [
     'call_to_action' => 'Du möchtest Abstimmen? Klicke Hier!',
     'errors' => [
-        'change_not_allowed' => 'Du bist nicht erlaubt den Vote zu ändern.',
+        'change_not_allowed' => 'Du bist nicht berechtigt den Vote zu ändern.',
     ],
 ];
