ebc630c6b3dca0e2007015faedd6ecf957de5536
Speidy674 <marvin33@live.de>
2026-08-15 15:51:03 +0200

    added whereConsideredStable to Vote Model

--- app/Models/Vote.php
+++ app/Models/Vote.php
@@ -4,7 +4,10 @@ declare(strict_types=1);
 
 namespace App\Models;
 
+use Carbon\CarbonInterval;
 use Database\Factories\VoteFactory;
+use Illuminate\Database\Eloquent\Attributes\Scope;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
